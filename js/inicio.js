34a6464a2ce29623c59e32d168bb233a0a010a2e
LGava <lgavagnin074@alumno.unlam.edu.ar>
2026-06-27 23:13:47 -0300

    inicio luego de iniciar sesion

---
+++ js/inicio.js
@@ -0,0 +1,39 @@
+
+document.getElementById("agendar").onclick=function(){
+
+    window.location="especialidad.html";
+};
+
+document.getElementById("turnos").onclick=function(){
+
+    window.location="misTurnos.html";
+};
+
+document.getElementById("historial").onclick=function(){
+
+    window.location="historial.html";
+};
+
+document.getElementById("datos").onclick=function(){
+
+    window.location="misDatos.html";
+};
+
+document.getElementById("notificaciones").onclick=function(){
+
+    window.location="notificaciones.html";
+};
+
+document.getElementById("ayuda").onclick=function(){
+
+    window.location="ayuda.html";
+};
+
+document.getElementById("cerrarSesion").onclick=function(){
+
+    let respuesta=confirm("¿Desea cerrar la sesión?");
+
+    if(respuesta){
+        window.location="index.html";
+    }
+};
